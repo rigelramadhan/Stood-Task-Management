05069de52465ced7264b3f89ce2e8864e39c39ea
Rigel Ramadhan <rigel.vibi@ai.astra.co.id>
2024-03-07 08:57:50 +0700

    [FEATURE][STOOD-72] Design system - Implement theme

--- features/common/src/main/java/one/reevdev/stood/features/common/theme/StoodTheme.kt
+++ features/common/src/main/java/one/reevdev/stood/features/common/theme/StoodTheme.kt
@@ -2,15 +2,29 @@ package one.reevdev.stood.features.common.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 
 @Composable
 fun StoodTheme(
     colorScheme: StoodColors = StoodTheme.colors,
     typography: StoodType = StoodTheme.types,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-
+    if (darkTheme) {
+        CompositionLocalProvider(
+            LocalDarkStoodColors provides colorScheme,
+            LocalDarkStoodType provides typography,
+            content = content,
+        )
+    } else {
+        CompositionLocalProvider(
+            LocalLightStoodColors provides colorScheme,
+            LocalLightStoodType provides typography,
+            content = content,
+        )
+    }
 }
 
 object StoodTheme {
